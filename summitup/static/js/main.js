902f644321ce33de0e63e3dcffe0850d766996b1
Rajinderpal Singh <60106112+sairish2001@users.noreply.github.com>
2022-09-04 04:24:59 +0530

    search results, ui created

--- summitup/static/js/main.js
+++ summitup/static/js/main.js
@@ -2,7 +2,13 @@ window.addEventListener('scroll',()=>{
     document.querySelector('nav').classList.toggle('window-scrolled',window.scrollY > 0)
 })
 
+function searchResults(){
+    document.getElementById("searchbarparent").style.padding=0;
 
+    document.getElementById("searchbarchild").style.marginTop=0;
+    
+    document.getElementById("results").style.display="block";
+}
 
 setTimeout(function(){
     $("#headingtop").fadeOut(300);
