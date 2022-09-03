8d4835886d38e47a82ca99f63615fc40d08a7e31
Rajinderpal Singh <60106112+sairish2001@users.noreply.github.com>
2022-09-03 18:49:12 +0530

    inital commit

---
+++ summitup/api/views.py
@@ -0,0 +1,5 @@
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import *
